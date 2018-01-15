c2ad26ccbeba2e70890666940905a896fd7c5536
mitchreece <mitchellreece18@gmail.com>
2018-01-15 23:42:27 +0000

    basic colour theming

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const { colors } = require('./styles/vars')
+
 exports.decorateConfig = (config) => {
     return Object.assign({}, config, {
+        borderColor: 'transparent',
+        backgroundColor: colors.primary,
+        foregroundColor: colors.fontLight,
+        cursorColor: colors.secondary,
         css: `
             ${config.css || ''}
         `
---
+++ styles/vars/index.js
@@ -0,0 +1,5 @@
+exports.colors = {
+    primary: '#100A11',
+    secondary: '#00A99D',
+    fontLight: '#EEEEEE',
+}
