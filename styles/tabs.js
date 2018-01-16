66504d9642e8f9f530c49f46c2116879056cf8f3
mitchreece <mitchellreece18@gmail.com>
2018-01-16 18:52:13 +0000

    improved tab styling on hover

--- styles/tabs.js
+++ styles/tabs.js
@@ -1,15 +1,28 @@
 const { colors } = require('./vars/')
 
 module.exports = `
-    .tabs_title {
+    .tab_tab {
+        transition: opacity .2s ease-in;
+        opacity: .5;
+    }
+
+    .tab_tab:hover {
+        transition: opacity .3s ease-in-out;
+        opacity: 1;
+    }
+
+    .tab_active,
+    .tabs_title
+    {
+        transition: opacity .2s ease-in;
+        opacity: .8;
         color: ${ colors.secondary };
     }
 
-    .tab_active {
-        color: ${ colors.secondary };
-    }
-
-    .tab_icon {
+    .tab_active .tab_icon,
+    .tab_active:hover,
+    .tab_icon:hover .tab_icon
+    {
         color: ${ colors.secondary };
     }
 `
